f8ac3cf07858082ca7ad4ca09cbf21c874307ce1
leuhsouza <36518285+leuhsouza@users.noreply.github.com>
2025-05-27 00:27:23 +0000

    fix: endereço

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -12,8 +12,8 @@ export const businessHours = {
 export const contactInfo = {
   whatsapp: "+5541999999999", // Replace with actual number
   instagram: "@a.casadomestre.cic",
-  address: "Cidade Industrial de Curitiba (CIC), Curitiba - PR", // Replace with actual address
-  mapUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d57713.333762478945!2d-49.361256078649866!3d-25.50009850505513!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94dce3836bfcf0a9%3A0xf5f3c0a8b18269!2sCidade%20Industrial%2C%20Curitiba%20-%20PR!5e0!3m2!1spt-BR!2sbr!4v1714937379761!5m2!1spt-BR!2sbr"
+  address: "R. Gilberto Kaminski, 157 - Cidade Industrial de Curitiba", // Replace with actual address
+  mapUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3600.702486996697!2d-49.3303538!3d-25.514967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94dcfd2eb748a0eb%3A0xff85ebefbd198312!2sR.%20Gilberto%20Kaminski%2C%20157%20-%20Cidade%20Industrial%20de%20Curitiba%2C%20Curitiba%20-%20PR%2C%2081170-260!5e0!3m2!1spt-BR!2sbr!4v1748305445760!5m2!1spt-BR!2sbr"
 };
 
 export const services = [
